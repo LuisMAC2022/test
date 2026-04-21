0db49d9ab585558b99d9783958d1edb6605d4f17
LuisMAC2022 <111030971+LuisMAC2022@users.noreply.github.com>
2026-04-21 08:32:02 +0000

    cat

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function resizeCanvasToScreen() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   context.fillStyle = 'teal';
-  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillRect(0, 0, canvas.width/2, canvas.height);
 }
 
 resizeCanvasToScreen();
