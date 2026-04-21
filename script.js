9000d4e57f4a126fe2b6f3c1e40eafdb0ecbc64e
LuisMAC2022 <111030971+LuisMAC2022@users.noreply.github.com>
2026-04-21 08:24:30 +0000

    miau

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const canvas = document.getElementById('screen-canvas');
+const canvas = document.getElementById('arenero');
 const context = canvas.getContext('2d');
 
 function resizeCanvasToScreen() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   context.fillStyle = 'teal';
-  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillRect(0, 0, canvas.width/2, canvas.height);
 }
 
 window.addEventListener('resize', resizeCanvasToScreen);
